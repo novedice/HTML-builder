c33ad999d78344702dd4136c986f2dba14718d99
Natalia <natalia.kitesurf@gmail.com>
2022-11-08 03:30:39 +0000

    fix: fix bug in 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,34 +2,49 @@
 function copyDir() {
     const path = require('path'),
           fs = require('fs');
-    fs.mkdir(path.join(__dirname, 'files-copy'),
-            {recursive: true}, (err) => {
-                if (err) {
-                    console.log(err);
-                };
-            });
-    fs.readdir(path.join(__dirname, 'files-copy'),
+
+    fs.access(path.join(__dirname,'files-copy'), (err) => {
+        if (err) {} else {
+            fs.readdir(path.join(__dirname, 'files-copy'),
         {withFileTypes: true},
         (err, files) => {
             if (err) {
-                console.log(err);
+                throw err;
             } else {
                 files.forEach(file => {
+                    fs.access(path.join(__dirname,'files', file.name), (err) => {
+                        if (err) {
                     fs.unlink(path.join(__dirname, 'files-copy', file.name), err => {
                         if (err) {console.log(err)};
                     })
+                }
+                }
+                    )
                 })
             }
         });
+           
+        }
+    })
+   
+    fs.mkdir(path.join(__dirname, 'files-copy'),
+            {recursive: true}, (err) => {
+                if (err) {
+                    // console.log(err);
+                };
+            });
+    
     fs.readdir(path.join(__dirname, 'files'), 
     {withFileTypes: true},
     (err, files) => {
         if (err) {
-            console.log(err);
+            // console.log(err);
         } else {
             files.forEach((file) => {
                 fs.copyFile(path.join(__dirname, 'files', file.name), path.join(__dirname, 'files-copy', file.name), err => {
-                    if (err) {console.log(err)};
+                    if (err) {
+                        // console.log(err)
+                    };
                 })
             })
         }
